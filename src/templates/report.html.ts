b813728ba6ef34ef0303d5262f6d46ebb4be2a0d
Guillaume Lethuillier <guillaume.lethuillier@ledger.fr>
2021-05-05 18:18:27 +0200

    feat(derivation mode): save in individual JSON files

--- src/templates/report.html.ts
+++ src/templates/report.html.ts
@@ -255,6 +255,7 @@ export const reportTemplate = `
         <li><strong>Analysis date:</strong> {analysis_date}</li>
         <li><strong>Provider:</strong> {provider} ({provider_url})</li>
         <li><strong>Gap limit:</strong> {gap_limit}</li>
+        <li><strong>Scan mode:</strong> {mode}</li>
         <li><strong>Xpub Scan version:</strong> {version}</li>
       </ul>
     </div>
