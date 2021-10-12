37012465e520734a7aa168df3c4bbd79411263a5
Guillaume Lethuillier <guillaume.lethuillier@ledger.fr>
2021-10-12 21:45:11 +0200

    feat(ci): move regression datasets

--- .github/workflows/regression_tests/check.py
+++ .github/workflows/regression_tests/check.py
@@ -55,10 +55,18 @@ def run_negative_test(data):
 
 
 if __name__ == "__main__":
+    product_under_test = sys.argv[1].lower().strip().replace('-', ' ')
+
     with open(f"{base_path}/datasets.json", 'r') as f:
         dataset = json.load(f)
 
     for data in dataset:
+
+        product = data['product'].lower().replace('-', ' ')
+
+        if product_under_test not in product:
+            continue
+
         test_types = data['test_types']
 
         for test_type in test_types:
